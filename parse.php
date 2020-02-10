c9d342846984213dc5f857f9bb025b6c40f1c7a0
xcoupe01 <vojtech.coupek@email.cz>
2020-02-10 16:40:29 +0100

    I can generate XML now

--- parse.php
+++ parse.php
@@ -3,7 +3,7 @@
    author : Vojtech Coupek - xcoupe01 */
 
 //----- definig variables -----
-$debug = true; // enables debug prints
+$debug = false; // enables debug prints
 define("ERR_OK", 0);        //< correct exit code
 define("ERR_HEADER", 21);   //< header fail
 define("ERR_OPCODE", 22);   //< operation code fail
@@ -54,12 +54,15 @@ $rules=[ //base functions
         'BREAK'       =>['params'=>[]],
         ];
 
-$state = ['numLoc'       => 0,                         //< num of lines with opcodes
-          'numComments'  => 0,                         //< num of comments
-          'numLabels'    => 0,                         //< number of labels
-          'numJumps'     => 0];                        //< num of jumps
+$state = ['numLoc'       => 0,                         //< num of lines with opcodes  --loc
+          'numComments'  => 0,                         //< num of comments            --comments
+          'numLabels'    => 0,                         //< number of labels           --labels
+          'numJumps'     => 0,                         //< num of jumps               --jumps
+          'order'        =>[]];                        //< order in stat file
 
-$output = []; //< output XML file that will be outputed if the whole input is correct
+$output = new DOMDocument("1.0", "UTF-8"); //< output XML file that will be outputed if the whole input is correct
+$outProgram = $output->createElement("program");
+$outProgram->setAttribute("language","IPPcode20");
 //----- used functions -----
 
 /**
@@ -77,7 +80,6 @@ function decho($toprint){
 * @return true if variable exists, false otherwise
 */
 function varCheck($str){
-  global $state;
   if(strpos($str, "@") !== false){
     $prefix = substr($str, 0, 2);
     $name = substr($str, 3);
@@ -93,55 +95,62 @@ function varCheck($str){
 /**
 * Checks if symbol is ok
 * @param str is symbol to be checked
-* @return true if symbol matches the requirements of symbol, false otherwise
+* @return type (int, bool, string, nil, var) if symbol matches the requirements of symbol, false otherwise
 */
 function symbCheck($str){
-  global $state;
   if(($cut = strpos($str, "@")) !== false){
     $prefix = substr($str, 0, $cut);
-    $value = substr($str, $cut + 1);
+    $name = substr($str, $cut + 1);
     if($prefix == "GF" || $prefix == "LF" || $prefix == "TF"){
       //i am variable
       decho(" symbCheck     \e[33mVARIABLE RECOGNIZED\e[0m passing [$str] to varCheck\n");
-      return varCheck($str);
+      if(!varCheck($str)){
+        return false;
+      }
+      return "var";
     } elseif($prefix == "int" || $prefix == "bool" || $prefix == "string" || $prefix == "nil"){
       //i am constant
       switch($prefix){
         case "int" :
-          if(preg_match("/^\d+$/", $value) || preg_match("/^-\d+$/", $value)){
+          if(preg_match("/^\d+$/", $name) || preg_match("/^-\d+$/", $name)){
             decho(" symbCheck     \e[32mIS SYMBOL\e[0m [$str]\n");
-            return true;
+            return "int";
           }
           break;
         case "bool" :
-          // can I use TRUE aswell ? ---------------------------------------------------- TODO
-          if($value == "true" || $value == "false"){
+          if($name == "true" || $name == "false"){
             decho(" symbCheck     \e[32mIS SYMBOL\e[0m [$str]\n");
-            return true;
+            return "bool";
           }
           break;
         case "string":
-          // check only escape sequences ?? --------------------------------------------- TODO
-          for($i = 0; $i < strlen($value); $i++){
-            if($value[$i] == "\\"){
+          for($i = 0; $i < strlen($name); $i++){
+            if($name[$i] == "\\"){
               //checking escape sequence - ascii now i guess
-              if(!(($value[$i + 1] <= '9' && $value[$i + 1] >= '0')
-                && ($value[$i + 2] <= '9' && $value[$i + 2] >= '0')
-                && ($value[$i + 3] <= '9' && $value[$i + 3] >= '0'))){
+              if(!(($name[$i + 1] <= '9' && $name[$i + 1] >= '0')
+                && ($name[$i + 2] <= '9' && $name[$i + 2] >= '0')
+                && ($name[$i + 3] <= '9' && $name[$i + 3] >= '0'))){
                   //failed escape sequence
                   decho(" symbCheck     \e[31mFAIL\e[0m [$str] bad escape sequence\n");
                   return false;
                 }
                 $i += 3;
+            } elseif($name[$i] == "&" || $name[$i] == "<" || $name[$i] == ">"){
+              $begin = substr($name, 0, $i);
+              $end = substr($name, $i+1);
+              if($name[$i] == "&") $name = $begin . "&amp" . $end;
+              if($name[$i] == "<") $name = $begin . "&lt" . $end;
+              if($name[$i] == ">") $name = $begin . "&gt" . $end;
+              $i += 2;
             }
           }
           decho(" symbCheck     \e[32mIS SYMBOL\e[0m [$str]\n");
-          return true;
+          return "string";
           break;
-        case "nill" :
-          if($value == "nill"){
+        case "nil" :
+          if($name == "nil"){
             decho(" symbCheck     \e[33mIS SYMBOL\e[0m [$str]\n");
-            return true;
+            return "nil";
           }
           break;
       }
@@ -159,6 +168,7 @@ function symbCheck($str){
 function labelCheck($str){
   if(preg_match("/^([^-\s]+)$/", $str)){
     decho(" labelCheck    \e[32mGOOD\e[0m [$str]\n");
+    $name = $str; $type = "label";
     return true;
   }
   decho(" labelCheck    \e[31mFAIL\e[0m [$str]\n");
@@ -166,28 +176,58 @@ function labelCheck($str){
 }
 
 //----- main script -----
-//add loading prog params --------------------------------------------------------------- TODO
+// dealing with parameters - help part
+if(count($argv) > 2){
+  foreach($argv as $value){
+    if($value == "--help"){
+      decho("\e[31mWRONG PARAMETERS\e[0m use just \"--help\" \n");
+      exit(10);
+    }
+  }
+} elseif(count($argv) == 2) {
+  if($argv[1] == "--help"){
+    echo "\n Filter type script. Expects intput of type IPPcode20 on standard input,checks if its syntax is correct and produce
+   XML representation of inputed program. Also it can make stat file.
+   Options :
+   \"--help\"           to print help info
+   \"--source=[file]\"  to set on the output stat file and give location
+   \"--loc\"            to mention number of lines in stat file
+   \"--comments\"       to mention number of comments in stat file
+   \"--labels\"         to mention number of labels in stat file
+   \"--jumps\"          to mention number of jumps in stat file
+   if you use soume of the last four, you must set source file!\n\n";
+    exit(ERR_OK);
+  }
+}
+
 //loading file
 $input=[];
-$i = 0;
 while (FALSE !== ($line = fgets(STDIN))){
   array_push($input,str_replace("\n", '', trim($line)));
 }
 decho("------\033[0;34m LOADED  INPUT \033[0;37m------\n");
-if($debug) print_r($input);
-//looking for the header
-if($input[0] != ".IPPcode20"){
-  //missing header
-  exit(ERR_HEADER);
-}
 //deleting all comments
-for($i=1; $i < count($input); $i++){
+for($i=0; $i < count($input); $i++){
   if(($cut = strpos($input[$i], "#")) !== false){
     //line has a comment
     $state['numComments'] ++;
     $input[$i] = substr($input[$i], 0, $cut);
+    trim($input[$i]);
   }
 }
+//looking for the header
+$i = 0; $go = true;
+do{
+  if($input[$i] == ""){
+    $i ++;
+  } elseif($input[$i] == ".IPPcode20"){
+    $go = false;
+  } else {
+    //missing header
+    decho("\e[31m MISSING HEADER\e[0m\n");
+    exit(ERR_HEADER);
+  }
+} while($go);
 decho("-----\033[0;34m TRIMMED COMENTS \033[0;37m-----\n");
 if($debug) print_r($input);
 //main checker
@@ -202,9 +242,9 @@ for($i=1; $i < count($input); $i++){
       $num = count($rules[$opcode]['params']);
       $regexp = "/^(\w+)";
       for($j = 0; $j < $num; $j++){
-        $regexp = $regexp . "\s+([^-\s]+)";
+        $regexp = $regexp . "\s+([^\s]+)";
       }
-      $regexp = $regexp . "$/";
+      $regexp = $regexp . "\s*$/";
       if(preg_match("$regexp", $input[$i], $m)){
         decho(" paramCheck    \e[32mGOOD\e[0m by [$regexp]\n");
       } else {
@@ -212,24 +252,53 @@ for($i=1; $i < count($input); $i++){
         decho(" paramCheck    \e[31mFAIL\e[0m by [$regexp]\n");
         exit(ERR_OTHER);
       }
+      $outInstruction = $output->createElement("instruction");
+      $outInstruction->setAttribute("order", $state['numLoc']);
+      $outInstruction->setAttribute("opcode", $opcode);
       $j = 2;
       foreach($rules[$opcode]['params'] as $value){
-        if($value == "var" && !varCheck($m[$j])){
-          decho(" opcode $opcode \e[31mFAIL\e[0m by varCheck at [$m[$j]]");
-          exit(ERR_OTHER);
-        } elseif($value == "symb" && !symbCheck($m[$j])){
-          decho(" opcode $opcode \e[31mFAIL\e[0m by symbCheck at [$m[$j]]");
-          exit(ERR_OTHER);
-        } elseif($value == "label" && !labelCheck($m[$j])){
-          decho(" opcode $opcode \e[31mFAIL\e[0m by labelCheck at [$m[$j]]");
-          exit(ERR_OTHER);
-        } elseif($value == "type" && ($m[$j] != "int" && $m[$j] != "bool" && $m[$j] != "string")){
-          decho(" opcode $opcode \e[31mFAIL\e[0m by typeCheck at [$m[$j]]");
-          exit(ERR_OTHER);
+        $name = "";
+        $type = "";
+        if($value == "var"){
+          if(!varCheck($m[$j])){
+            decho(" opcode $opcode \e[31mFAIL\e[0m by varCheck at [$m[$j]]");
+            exit(ERR_OTHER);
+          }
+          $type = "var";
+          $name = $m[$j];
+        } elseif($value == "symb"){
+          if(($type = symbCheck($m[$j])) === false){
+            decho(" opcode $opcode \e[31mFAIL\e[0m by symbCheck at [$m[$j]]");
+            exit(ERR_OTHER);
+          }
+          if($type == "var"){
+            $name = $m[$j];
+          } else {
+            $cut = strpos($m[$j], "@");
+            $name = substr($m[$j], $cut + 1);
+          }
+        } elseif($value == "label"){
+          if( !labelCheck($m[$j])){
+            decho(" opcode $opcode \e[31mFAIL\e[0m by labelCheck at [$m[$j]]");
+            exit(ERR_OTHER);
+          }
+          $type = "label";
+          $name = $m[$j];
+        } elseif($value == "type"){
+          $name = $m[$j]; $type = "type";
+          if($m[$j] != "int" && $m[$j] != "bool" && $m[$j] != "string"){
+            decho(" opcode $opcode \e[31mFAIL\e[0m by typeCheck at [$m[$j]]");
+            exit(ERR_OTHER);
+          }
         }
+        $outArgument = $output->createElement("arg" . ($j -1) );
+        $outArgument->setAttribute("type", $type);
+        $outArgument->nodeValue = $name;
+        $outInstruction->appendChild($outArgument);
         $j++;
       }
-      if($opcode == "JUMP" || $opcode == "JUMPIFEQ" || $opcode == "JUMPINEQ"){
+      $outProgram->appendChild($outInstruction);
+      if($opcode == "JUMP" || $opcode == "JUMPIFEQ" || $opcode == "JUMPIFNEQ" || $opcode == "CALL"){
         $state['numJumps'] ++;
       }
       if($opcode == "LABEL"){
@@ -237,6 +306,7 @@ for($i=1; $i < count($input); $i++){
       }
     } else {
       //opcode not found in rules
+     decho("\e[31m BAD OPCODE\e[0m\n");
       exit(ERR_OPCODE);
     }
   } else {
@@ -244,6 +314,58 @@ for($i=1; $i < count($input); $i++){
   }
 }
 decho("------\033[0;34m MAIN END \033[0;37m-------\n");
+//dealing with parameters - stat part
+$isfileset = false;
+if(count($argv) > 1){
+  foreach($argv as $value){
+    if($value == "parse.php"){
+
+    } elseif($value == "--loc" || $value == "--comments" || $value == "--labels" || $value == "--jumps"){
+      array_push($state['order'], $value);
+    } elseif(preg_match("/^--stats=(\w*.*)$/", $value, $m) !== false ||
+              preg_match("/^--stats=\"(\w*.*)\"$/", $value, $m) !== false) {
+          if($isfileset){
+            decho("\e[31m MULTIPLE STATFILE PLACES\e[0m\n");
+            exit(10);
+          } else {
+            $statfile = $m[1];
+            $isfileset = true;
+          }
+    } else {
+      decho("\e[31m UNKNOWN OPTION\e[0m\n");
+      // skip or let go ??
+    }
+  }
+  if(!$isfileset){
+    decho("\e[31m NO FILE SET\e[0m\n");
+    exit(10);
+  }
+  if(($handle = fopen($statfile, "w")) === false){
+    exit(99);
+  }
+
+  foreach($state['order'] as $value){
+    switch($value){
+      case "--loc":
+        fprintf($handle, "%d\n", $state['numLoc']);
+        break;
+      case "--comments":
+        fprintf($handle, "%d\n", $state['numComments']);
+        break;
+      case "--labels":
+        fprintf($handle, "%d\n", $state['numLabels']);
+        break;
+      case "--jumps":
+        fprintf($handle, "%d\n", $state['numJumps']);
+        break;
+      default:
+        exit(99);
+    }
+  }
+  fclose($handle);
+}
 if($debug) print_r($state);
+$output->appendChild($outProgram);
+print $output->saveXML();
 exit(ERR_OK);
 ?>
